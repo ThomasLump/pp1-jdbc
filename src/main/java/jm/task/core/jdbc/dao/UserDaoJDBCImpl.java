46ec65fafa91795fe55cc42567d9515cc286d1a8
ThomasLump <thomaslump@hotmail.com>
2024-12-19 20:29:04 +0300

    Init

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +13,82 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE user (" +
+                "id BIGINT AUTO_INCREMENT PRIMARY KEY," +
+                "name VARCHAR(255) NOT NULL," +
+                "lastname VARCHAR(255) NOT NULL," +
+                "age TINYINT UNSIGNED NOT NULL)";
+        Util.executeWithConnection(connection -> {
+            Util.executeWithStatement(connection, statement -> {
+                statement.execute(sql);
+            });
+        });
     }
 
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE user";
+        Util.executeWithConnection(connection -> {
+            Util.executeWithStatement(connection, statement -> {
+                statement.execute(sql);
+            });
+        });
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO user (name,lastname,age) VALUES (?,?,?)";
+        Util.executeWithConnection(connection -> {
+            Util.executeWithPreparedStatement(connection, sql, statement -> {
+                statement.setString(1, name);
+                statement.setString(2, lastName);
+                statement.setByte(3, age);
+                statement.executeUpdate();
+            });
+        });
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM user WHERE id = ?";
+        Util.executeWithConnection(connection -> {
+            Util.executeWithPreparedStatement(connection, sql, statement -> {
+                statement.setLong(1, id);
+                statement.executeUpdate();
+            });
+        });
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM user";
+        List<User> users = new ArrayList<>();
+        return Util.executeWithConnection(connection -> {
+            return Util.executeWithStatement(connection, statement -> {
+                try (ResultSet resultSet = statement.executeQuery(sql)) {
+                    while (resultSet.next()) {
+                        /*users.add(new User(
+                                resultSet.getString("name"),
+                                resultSet.getString("lastname"),
+                                resultSet.getByte("age")
+                        ));*/
+                        //как бы сюда айди присрать еще, не делая новый конструктор
+                        User addedUser = new User(
+                                resultSet.getString("name"),
+                                resultSet.getString("lastname"),
+                                resultSet.getByte("age")
+                        );
+                        addedUser.setId(resultSet.getLong("id"));
+                        users.add(addedUser);
+                    }
+                    return users;
+                }
+            });
+        });
     }
 
     public void cleanUsersTable() {
-
+        String sql = "DELETE FROM user";
+        Util.executeWithConnection(connection -> {
+            Util.executeWithStatement(connection, statement -> {
+                statement.execute(sql);
+            });
+        });
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,69 @@
 package jm.task.core.jdbc.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final DataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:7777/mysql");
+        config.setUsername("root");
+        config.setPassword("");
+        dataSource = new HikariDataSource(config);
+    }
+
+    public interface SQLConsumer<T> {
+        void accept(T t) throws SQLException;
+    }
+
+    public interface SQLFunction<T, R> {
+        R apply(T t) throws SQLException;
+    }
+
+    public static void executeWithConnection(SQLConsumer<Connection> cons) {
+        try (Connection connection = dataSource.getConnection()) {
+            cons.accept(connection);
+        } catch (Exception e) {
+        }
+    }
+
+    public static <R> R executeWithConnection(SQLFunction<Connection, ? extends R> func) {
+        try (Connection connection = dataSource.getConnection()) {
+            return func.apply(connection);
+        } catch (Exception e) {
+        }
+        return null;//может стоит заворачивать в опционал?...
+    }
+
+    public static void executeWithStatement(Connection connection, SQLConsumer<Statement> cons) {
+        try (Statement statement = connection.createStatement()) {
+            cons.accept(statement);
+        } catch (Exception e) {
+        }
+    }
+
+    public static <R> R executeWithStatement(Connection connection, SQLFunction<Statement, ? extends R> func) {
+        try (Statement statement = connection.createStatement()) {
+            return func.apply(statement);
+        } catch (Exception e) {}
+        return null;
+    }
+
+    public static void executeWithPreparedStatement(Connection connection, String sql, SQLConsumer<PreparedStatement> cons) {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            cons.accept(statement);
+        } catch (Exception e) {
+        }
+    }
+
+
 }
